f13e34ca1dbb34f8cf1d7cc30bf257353c37ba40
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-08-06 15:23:11 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,26 @@
+def power(base_num, power_num):
+    result = 1
+    for index in range(power_num):
+        result = result * base_num
+    
+    return result
+print(power(2,10))    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
